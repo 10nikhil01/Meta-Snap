bad469db0c582036ff1e9be8b45af9e00c5ee22d
10nikhil01 <nikhilcreationshere@gmail.com>
2025-09-30 17:21:20 +0530

    fix pwa next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 // next.config.js
 import { withPWA } from "next-pwa";
-import runtimeCaching from "next-pwa/cache";
+import runtimeCaching from "next-pwa/cache.js";
 
 const isProd = process.env.NODE_ENV === "production";
 
