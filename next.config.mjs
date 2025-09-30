8c07f0b17b25ea54837fc38322508105dace0f6c
10nikhil01 <nikhilcreationshere@gmail.com>
2025-09-30 17:24:21 +0530

    fix pwa next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
-// next.config.js
 import { withPWA } from "next-pwa";
-import runtimeCaching from "next-pwa/cache.js";
 
 const isProd = process.env.NODE_ENV === "production";
 
@@ -8,6 +6,6 @@ export default withPWA({
   pwa: {
     dest: "public",
     disable: !isProd,
-    runtimeCaching,
+    
   },
 });
